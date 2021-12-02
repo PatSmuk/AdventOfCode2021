7cbbfdd42275ce5cfd3bd34a30d679e67685c22c
Pat Smuk <pat.smuk@gmail.com>
2021-12-02 00:11:54 -0500

    Day 2

--- d02/p1/index.ts
+++ d02/p1/index.ts
@@ -1,8 +1,25 @@
 import { readInputFileLines } from '../../util'
 
 function parseLine(line: string) {
-  return line
+  const [direction, amount] = line.split(' ')
+  return { direction, amount: parseInt(amount) }
 }
 
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+
+let x = 0
+let y = 0
+
+for (const { direction, amount } of inputs) {
+  if (direction === 'forward') {
+    x += amount
+  }
+  if (direction === 'down') {
+    y += amount
+  }
+  if (direction === 'up') {
+    y -= amount
+  }
+}
+
+console.log(x, y, x * y)
--- d02/p2/index.ts
+++ d02/p2/index.ts
@@ -1,8 +1,28 @@
 import { readInputFileLines } from '../../util'
 
 function parseLine(line: string) {
-  return line
+  const [direction, amount] = line.split(' ')
+  return { direction, amount: parseInt(amount) }
 }
 
+
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+
+let x = 0
+let y = 0
+let aim = 0
+
+for (const { direction, amount } of inputs) {
+  if (direction === 'forward') {
+    x += amount
+    y += aim * amount
+  }
+  if (direction === 'down') {
+    aim += amount
+  }
+  if (direction === 'up') {
+    aim -= amount
+  }
+}
+
+console.log(x, y, x * y)
