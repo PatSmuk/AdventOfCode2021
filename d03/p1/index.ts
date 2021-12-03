8aaf9343f095b0cb279b21541718918fdc28f8a5
Pat Smuk <pat.smuk@gmail.com>
2021-12-03 00:37:30 -0500

    Day 3

--- d03/p1/index.ts
+++ d03/p1/index.ts
@@ -5,4 +5,24 @@ function parseLine(line: string) {
 }
 
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+
+let gammaString = ''
+let ones = '' // will be ('1' * inputs[0].length)
+
+// For each character position...
+for (let i = 0; i < inputs[0].length; i++) {
+  // Count the zeros
+  let zeros = 0
+  for (const line of inputs) {
+    if (line[i] === '0') zeros++
+  }
+
+  // Check if zeros are more common
+  gammaString += (zeros > inputs.length / 2) ? '0' : '1'
+  ones += '1'
+}
+
+const gamma = parseInt(gammaString, 2)
+// Epsilon is just gamma with the bits flipped
+const epsilon = gamma ^ parseInt(ones, 2)
+console.log(gamma, epsilon, gamma * epsilon)
--- d03/p2/index.ts
+++ d03/p2/index.ts
@@ -5,4 +5,35 @@ function parseLine(line: string) {
 }
 
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+
+let numbers = inputs
+// For each character position...
+for (let i = 0; i < numbers[0].length && numbers.length > 1; i++) {
+  // Count the zeros
+  let zeros = 0
+  for (const line of numbers) {
+    if (line[i] === '0') zeros++
+  }
+
+  // Check if zeros are more common
+  const wanted = (zeros > numbers.length / 2) ? '0' : '1'
+  numbers = numbers.filter(n => n[i] === wanted)
+}
+const a = parseInt(numbers[0], 2)
+
+numbers = inputs
+// For each character position...
+for (let i = 0; i < numbers[0].length && numbers.length > 1; i++) {
+  // Count the zeros
+  let zeros = 0
+  for (const line of numbers) {
+    if (line[i] === '0') zeros++
+  }
+
+  // Check if zeros are more common
+  const wanted = (zeros > numbers.length / 2) ? '1' : '0'
+  numbers = numbers.filter(n => n[i] === wanted)
+}
+const b = parseInt(numbers[0], 2)
+
+console.log(a, b, a * b)
