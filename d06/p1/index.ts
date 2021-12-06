4fd0ae8f474c00046f2daaf784faf3f2a4ad3ec7
Pat Smuk <pat.smuk@gmail.com>
2021-12-06 00:31:30 -0500

    Day 6

--- d06/p1/index.ts
+++ d06/p1/index.ts
@@ -1,8 +1,33 @@
 import { readInputFileLines } from '../../util'
 
 function parseLine(line: string) {
-  return line
+  return line.split(',').map(x => parseInt(x))
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+// Keep track of the age of each fish in an array.
+const fishAges = readInputFileLines(__dirname, parseLine)[0]
+
+const DAYS = 80
+
+// For each day...
+for (let day = 0; day < DAYS; day++) {
+  let newFish = 0
+
+  // Go through each existing fish.
+  for (let i = 0; i < fishAges.length; i++) {
+    fishAges[i]--
+
+    // If the age was 0, reset it to 6 and increase spawn counter.
+    if (fishAges[i] === -1) {
+      fishAges[i] = 6
+      newFish++
+    }
+  }
+
+  // Spawn new fish with a timer of 8.
+  for (let i = 0; i < newFish; i++) {
+    fishAges.push(8)
+  }
+}
+
+console.log(fishAges.length)
--- d06/p2/index.ts
+++ d06/p2/index.ts
@@ -1,8 +1,37 @@
 import { readInputFileLines } from '../../util'
 
 function parseLine(line: string) {
-  return line
+  return line.split(',').map(x => parseInt(x))
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+// Create map from time left to number of fish.
+const fishByTimer = new Map<number, bigint>()
+for (let i = 0; i <= 8; i++) {
+  fishByTimer.set(i, 0n)
+}
+
+// Initialize the count of fish by age using input data.
+const inputs = readInputFileLines(__dirname, parseLine)[0]
+for (const input of inputs) {
+  fishByTimer.set(input, fishByTimer.get(input)! + 1n)
+}
+
+const DAYS = 256
+
+// For each day...
+for (let day = 0; day < DAYS; day++) {
+  // Determine number of fish to spawn.
+  let fishToSpawn = fishByTimer.get(0)!
+
+  // Move all the fish down one key.
+  for (let i = 0; i < 8; i++) {
+    fishByTimer.set(i, fishByTimer.get(i + 1)!)
+  }
+
+  // Spawn new fish with a timer of 8, and add their parents back to the 6 timer bucket.
+  fishByTimer.set(8, fishToSpawn)
+  fishByTimer.set(6, fishByTimer.get(6)! + fishToSpawn)
+}
+
+// Count up all the values for the total.
+console.log([...fishByTimer.values()].reduce((prev, curr) => prev + curr, 0n))
