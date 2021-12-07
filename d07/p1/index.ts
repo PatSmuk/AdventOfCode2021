5362c639c53b178f8aa51dfcc5e0e8a4b6754ff7
Pat Smuk <pat.smuk@gmail.com>
2021-12-07 00:31:52 -0500

    Day 7

--- d07/p1/index.ts
+++ d07/p1/index.ts
@@ -1,8 +1,24 @@
 import { readInputFileLines } from '../../util'
 
 function parseLine(line: string) {
-  return line
+  return line.split(',').map(x => parseInt(x))
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+const startPositions = readInputFileLines(__dirname, parseLine)[0]
+const maxPosition = Math.max(...startPositions)
+
+let smallestDiff = Number.POSITIVE_INFINITY
+// Try each position to find the smallest diff
+for (let position = 0; position <= maxPosition; position++) {
+  let diffs = 0
+
+  for (const input of startPositions) {
+    diffs += Math.abs(input - position)
+  }
+
+  if (diffs < smallestDiff) {
+    smallestDiff = diffs
+  }
+}
+
+console.log(smallestDiff)
--- d07/p2/index.ts
+++ d07/p2/index.ts
@@ -1,8 +1,32 @@
 import { readInputFileLines } from '../../util'
 
 function parseLine(line: string) {
-  return line
+  return line.split(',').map(x => parseInt(x))
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+const startPositions = readInputFileLines(__dirname, parseLine)[0]
+const maxPosition = Math.max(...startPositions)
+
+// Pre-compute the fuel required for each possible position difference.
+const fuelByDifference = [0]
+let fuelRequired = 1
+for (let i = 1; i < maxPosition; i++) {
+  fuelByDifference.push(fuelByDifference[i - 1] + fuelRequired)
+  fuelRequired++
+}
+
+let smallestDiff = Number.POSITIVE_INFINITY
+// Try each position to find the smallest diff
+for (let position = 0; position <= maxPosition; position++) {
+  let diffs = 0
+
+  for (const input of startPositions) {
+    diffs += fuelByDifference[Math.abs(input - position)]
+  }
+
+  if (diffs < smallestDiff) {
+    smallestDiff = diffs
+  }
+}
+
+console.log(smallestDiff)
