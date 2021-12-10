832e2a46bb1f6dd833f494961df388a562f4201b
Pat Smuk <pat.smuk@gmail.com>
2021-12-10 00:33:49 -0500

    Day 10

--- d10/p1/index.ts
+++ d10/p1/index.ts
@@ -1,8 +1,32 @@
 import { readInputFileLines } from '../../util'
 
-function parseLine(line: string) {
-  return line
+const PAIRS = ["()", "{}", "[]", "<>"]
+const POINTS_BY_CHAR: {[char: string]: number} = {")": 3, "]": 57, "}": 1197, ">": 25137}
+
+const inputs = readInputFileLines(__dirname, x => x)
+let points = 0
+
+nextLine:
+for (const line of inputs) {
+  const expectedChars = []
+
+  for (let i = 0; i < line.length; i++) {
+    const char = line[i]
+    for (const pair of PAIRS) {
+      if (char === pair[0]) {
+        // The next expected end char is the one that matches this one.
+        expectedChars.push(pair[1])
+
+      } else if (char === pair[1]) {
+        // If this end char doesn't match the next expected end char,
+        //   the line is invalid.
+        if (char !== expectedChars.pop()) {
+          points += POINTS_BY_CHAR[pair[1]]
+          continue nextLine
+        }
+      }
+    }
+  }
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+console.log(points)
--- d10/p2/index.ts
+++ d10/p2/index.ts
@@ -1,8 +1,49 @@
 import { readInputFileLines } from '../../util'
 
-function parseLine(line: string) {
-  return line
+const inputs = readInputFileLines(__dirname, x => x)
+
+const PAIRS = ["()", "{}", "[]", "<>"]
+const POINTS = {")": 1, "]": 2, "}": 3, ">": 4} as {[x: string]: number}
+
+let pointsByLine =  []
+
+for (const line of inputs) {
+  const expectedChars = []
+  let invalid = false
+
+eachChar:
+  for (let i = 0; i < line.length; i++) {
+    const char = line[i]
+
+    for (const pair of PAIRS) {
+      if (char === pair[0]) {
+        // The next expected end char is the one that matches this one.
+        expectedChars.push(pair[1])
+
+      } else if (char === pair[1]) {
+        // If this end char doesn't match the next expected end char,
+        //   the line is invalid.
+        if (char !== expectedChars.pop()) {
+          invalid = true
+          break eachChar
+        }
+      }
+    }
+  }
+
+  // Skip invalid lines.
+  if (invalid) continue
+
+  expectedChars.reverse()
+  let points = 0
+  for (const char of expectedChars) {
+    points *= 5
+    points += POINTS[char]
+  }
+
+  pointsByLine.push(points)
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+// Sort them and take the middle one.
+pointsByLine.sort((a, b) => a - b)
+console.log(pointsByLine[Math.floor(pointsByLine.length / 2)])
