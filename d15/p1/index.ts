f0313172dcdb620a627e391dda86fa52ebbd1fef
Pat Smuk <pat.smuk@gmail.com>
2021-12-16 00:55:11 -0500

    Day 15

--- d15/p1/index.ts
+++ d15/p1/index.ts
@@ -1,8 +1,48 @@
-import { readInputFileLines } from '../../util'
+import { readInputFileLines, aStarSearch } from '../../util'
 
 function parseLine(line: string) {
-  return line
+  return line.split('').map(x => parseInt(x))
 }
 
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+const rows = inputs.length
+const cols = inputs[0].length
+const start = 0
+const end = (rows * cols) - 1
+
+function splitKey(key: number) {
+  const col = key % cols
+  const row = (key - col) / cols
+  return [row, col]
+}
+
+function manhattan(key: number) {
+  const [row, col] = splitKey(key)
+  return (rows - row - 1) + (cols - col - 1)
+}
+
+function getWeight(key: number) {
+  const [row, col] = splitKey(key)
+  return inputs[row][col]
+}
+
+function getNeighbours(key: number) {
+  const [row, col] = splitKey(key)
+  const neighbours = []
+  if (row > 0)        neighbours.push((row - 1) * cols + col)
+  if (row < rows - 1) neighbours.push((row + 1) * cols + col)
+  if (col > 0)        neighbours.push(row  * cols + col - 1)
+  if (col < cols - 1) neighbours.push(row  * cols + col + 1)
+  return neighbours
+}
+
+const shortestPath = aStarSearch({
+  start,
+  end,
+  getDistance: manhattan,
+  getWeight,
+  getNeighbours
+}).filter(node => node !== start)
+
+const totalScore = shortestPath.map(node => getWeight(node)).reduce((prev, curr) => prev + curr, 0)
+console.log(totalScore)
--- d15/p2/index.ts
+++ d15/p2/index.ts
@@ -1,8 +1,59 @@
-import { readInputFileLines } from '../../util'
+import { readInputFileLines, aStarSearch } from '../../util'
 
 function parseLine(line: string) {
-  return line
+  return line.split('').map(x => parseInt(x))
 }
 
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+let rows = inputs.length
+let cols = inputs[0].length
+
+// Virtual rows and columns, after 5x5 duplication.
+const vRows = rows * 5
+const vCols = cols * 5
+
+const start = 0
+const end = (vRows * vCols) - 1
+
+function splitKey(key: number) {
+  const col = key % vCols
+  const row = (key - col) / vCols
+  return [row, col]
+}
+
+function manhattan(key: number) {
+  const [row, col] = splitKey(key)
+  return (vRows - row - 1) + (vCols - col - 1)
+}
+
+function getWeight(key: number) {
+  const [row, col] = splitKey(key)
+  const vRowId = Math.floor(row / rows)
+  const rRowId = row % rows
+  const vColId = Math.floor(col / cols)
+  const rColId = col % cols
+  let weight = inputs[rRowId][rColId] + vRowId + vColId
+  while (weight > 9) weight -= 9
+  return weight
+}
+
+function getNeighbours(key: number) {
+  const [row, col] = splitKey(key)
+  const neighbours = []
+  if (row > 0)          neighbours.push((row - 1) * vCols + col)
+  if (row < vRows - 1)  neighbours.push((row + 1) * vCols + col)
+  if (col > 0)          neighbours.push(row  * vCols + col - 1)
+  if (col < vCols - 1)  neighbours.push(row  * vCols + col + 1)
+  return neighbours
+}
+
+const shortestPath = aStarSearch({
+  start,
+  end,
+  getDistance: manhattan,
+  getWeight,
+  getNeighbours
+}).filter(node => node !== start)
+
+const totalScore = shortestPath.map(node => getWeight(node)).reduce((prev, curr) => prev + curr, 0)
+console.log(totalScore)
