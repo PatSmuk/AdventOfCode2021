d0e2d1b003d22b3bcff989ca4c5d98c5ae6f6026
Pat Smuk <pat.smuk@gmail.com>
2021-12-17 00:39:25 -0500

    Day 17

--- d17/p1/index.ts
+++ d17/p1/index.ts
@@ -1,8 +1,44 @@
 import { readInputFileLines } from '../../util'
 
+const PATTERN = /target area: x=(.+), y=(.+)/
+
 function parseLine(line: string) {
-  return line
+  const match = PATTERN.exec(line)!
+  const x = match[1].split('..').map(x => parseInt(x))
+  const y = match[2].split('..').map(x => parseInt(x))
+  return [x, y]
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+const [, yRange] = readInputFileLines(__dirname, parseLine)[0]
+
+function simulateY(dy: number): [boolean, number] {
+  let y = 0
+  let maxY = Number.NEGATIVE_INFINITY
+
+  for (;;) {
+    y += dy
+    dy--
+
+    if (y > maxY) {
+      maxY = y
+    }
+
+    if (y >= yRange[0] && y <= yRange[1]) {
+      return [true, maxY]
+    }
+
+    if (y < yRange[0]) {
+      return [false, maxY]
+    }
+  }
+}
+
+let maxY = Number.NEGATIVE_INFINITY
+for (let dy = 1; dy <= 1000; dy++) {
+  const [result, y] = simulateY(dy)
+  if (result && y > maxY) {
+    maxY = y
+  }
+}
+
+console.log(maxY)
--- d17/p2/index.ts
+++ d17/p2/index.ts
@@ -1,8 +1,69 @@
 import { readInputFileLines } from '../../util'
 
+const PATTERN = /target area: x=(.+), y=(.+)/
+
 function parseLine(line: string) {
-  return line
+  const match = PATTERN.exec(line)!
+  const x = match[1].split('..').map(x => parseInt(x))
+  const y = match[2].split('..').map(x => parseInt(x))
+  return [x, y]
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+const [xRange, yRange] = readInputFileLines(__dirname, parseLine)[0]
+
+function simulateXAndY(dx: number, dy: number): boolean {
+  let x = 0
+  let y = 0
+
+  for (;;) {
+    x += dx
+    y += dy
+
+    if (dx > 0) {
+      dx--
+    }
+
+    dy--
+
+    if (x >= xRange[0] && x <= xRange[1] && y >= yRange[0] && y <= yRange[1]) {
+      return true
+    }
+
+    if (y < yRange[0]) {
+      return false
+    }
+  }
+}
+
+function simulateX(dx: number): boolean {
+  let x = 0
+
+  for (;;) {
+    x += dx
+
+    if (dx > 0) {
+      dx--
+    }
+
+    if (x >= xRange[0] && x <= xRange[1]) {
+      return true
+    }
+
+    if (x > xRange[1] || dx === 0) {
+      return false
+    }
+  }
+}
+
+const validVelocities = []
+for (let dx = 1; dx <= 1000; dx++) {
+  if (simulateX(dx)) {
+    for (let dy = -1000; dy <= 1000; dy++) {
+      if (simulateXAndY(dx, dy)) {
+        validVelocities.push([dx, dy])
+      }
+    }
+  }
+}
+
+console.log(validVelocities.length)
